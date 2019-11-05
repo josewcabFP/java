3470ac59db2deb8e6ae98ab2da04fcc4e2bd8012
Jose <josewcab.fp@gmail.com>
2019-11-05 13:27:38 +0100

    ejercicio fechas

---
+++ eclipse/tema3/src/Ej14_ED/Fecha.java
@@ -0,0 +1,115 @@
+package Ej14_ED;
+
+public class Fecha {
+	private int dia;
+	private int mes;
+	private int anyo;
+	
+	public Fecha (int _dia, int _mes, int _anyo) {
+		this.dia = _dia;
+		this.mes = _mes;
+		this.anyo = _anyo;
+	}
+	
+	public void diferencia(Fecha _otraFecha) {
+		
+		//podemos inicializar los valores como si el mes el dia y el año fueran mayores y luego
+		//solo especificar los casos en los que sean mayores.
+		int diasDif = Math.abs(this.dia - _otraFecha.getDia());
+		int mesDif = Math.abs(this.mes - _otraFecha.getMes());
+		int anyoDif = Math.abs(this.anyo - _otraFecha.getAnyo());
+		
+		
+		if (this.mes < _otraFecha.getMes()) {
+			mesDif = 12 - mesDif;
+			anyoDif = anyoDif - 1;
+		}
+		
+		if (this.dia < _otraFecha.getDia()) {
+			diasDif = this.diasMes(_otraFecha.getMes()) - diasDif;
+			mesDif = mesDif - 1;
+		}
+		
+		
+		
+		/*
+		 * if (this.anyo >= _otraFecha.getAnyo()){
+		 * 
+		 * if (this.mes >= _otraFecha.getMes()) {
+		 * 
+		 * mesDif = this.mes - _otraFecha.getMes();
+		 * 
+		 * if (this.dia >= _otraFecha.getDia()) { diasDif = this.dia -
+		 * _otraFecha.getDia(); } else { //dia diasDif =
+		 * this.diasMes(_otraFecha.getMes()) - Math.abs(this.dia - _otraFecha.getDia());
+		 * mesDif = mesDif - 1; }
+		 * 
+		 * } else { //mes mesDif = 12 - Math.abs(this.mes - _otraFecha.getMes());
+		 * 
+		 * if (this.mes >) } anyoDif = this.anyo - _otraFecha.getAnyo(); } else { //año
+		 * }
+		 */
+		
+		System.out.println("La diferencia es de " + anyoDif + " años, " + mesDif + " meses y " + diasDif + " dias.");
+	}
+	
+	public int diasMes(int mes) {
+		int dias;
+		
+		switch(mes) {
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 12:
+			dias = 31;
+			break;
+		case 2: 
+			dias = 29;
+			break;
+		default:
+			dias = 30;
+			break;
+		}
+		
+		return dias;
+	}
+	
+	public boolean bisiesto() {
+		boolean bis = false;
+		
+		if ((this.anyo % 4 == 0 || this.anyo % 400 == 0) && this.anyo % 100 != 0) {
+			bis = true;
+		}
+		
+		return bis;
+	}
+
+	public int getDia() {
+		return dia;
+	}
+
+	public void setDia(int dia) {
+		this.dia = dia;
+	}
+
+	public int getMes() {
+		return mes;
+	}
+
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
+
+	public int getAnyo() {
+		return anyo;
+	}
+
+	public void setAnyo(int anyo) {
+		this.anyo = anyo;
+	}
+	
+	
+
+}
---
+++ eclipse/tema3/src/Ej14_ED/TestFecha.java
@@ -0,0 +1,13 @@
+package Ej14_ED;
+
+public class TestFecha {
+
+	public static void main(String[] args) {
+		Fecha fech1 = new Fecha(4, 3, 1991); //mayor
+		Fecha fech2 = new Fecha(5, 4, 1990);
+		
+		fech1.diferencia(fech2);
+
+	}
+
+}
